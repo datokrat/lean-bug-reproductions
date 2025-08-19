8267f02e176859e62e95600a9997435eec61870b
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-08-19 15:05:59 +0200

    add error message

--- Reproductions/MacroRules2.lean
+++ Reproductions/MacroRules2.lean
@@ -1,3 +1,6 @@
 module
 
+/- Error:
+Unknown constant `_private.Reproductions.MacroRules1.0._aux_Reproductions_MacroRules1___macroRules_tacticGet_elem_tactic_extensible_1`
+-/
 public import Reproductions.MacroRules1
