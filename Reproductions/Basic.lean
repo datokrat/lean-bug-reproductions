5bc6a8c78935717f095c3a95f99dce80cadf577e
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-12-29 01:28:27 +0100

    repro

--- Reproductions/Basic.lean
+++ Reproductions/Basic.lean
@@ -1,8 +1,3 @@
 module
 
-import Reproductions.Dependency
-
-/-- error: Unknown constant `_private.Reproductions.Dependency.0.t` -/
-#guard_msgs in
-theorem u (h : m = n) : f m = f n := by
-  simp [h]
+public import Reproductions.Dependency
--- Reproductions/Dependency.lean
+++ Reproductions/Dependency.lean
@@ -1,6 +1,3 @@
 module
 
-public def f : Nat → Nat := sorry
-
-@[congr]
-theorem t (h : m = n) : f m = f n := sorry
+declare_syntax_cat ebnf
